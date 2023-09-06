17b39391acf7c76f11f42817d10efd1b3eb29516
Alex Sobral <alex.dacostasobral@gmail.com>
2023-09-06 14:59:10 -0300

    feat: add categories and articles

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const bodyParser = require('body-parser')
 const connection = require('./database/database');
 
+const categoriesController = require('./categories/CategoriesController')
+const articlesController = require('./articles/ArticlesController');
+
+const Article = require('./articles/Article'); 
+const Category = require('./categories/Category');
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -11,8 +17,11 @@ app.use(bodyParser.json());
 
 connection
   .authenticate()
-  .then(() => console.log('foi boy'))
+  .then(() => {})
   .catch((error) => console.log(error))
+
+app.use('/', categoriesController);
+app.use('/', articlesController);
 
 app.get('/', (req, res) => {
   res.render('index')
