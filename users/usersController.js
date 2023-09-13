abc32ced2ada28b90a7edf932f4440ee5b2476d4
Alex Sobral <alex.dacostasobral@gmail.com>
2023-09-13 11:00:40 -0300

    more secure user password - bcryptjs installed

--- users/usersController.js
+++ users/usersController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const user = require('./user');
+const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res) => {
   res.send('users');
@@ -14,7 +15,15 @@ router.post('/users/create', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  res.json({email, password});
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+
+  user.create({
+    email: email,
+    password: hash
+  })
+  .then(() =>res.redirect('/'))
+  .catch((err) => res.redirect('/'))
 })
 
 module.exports = router;
